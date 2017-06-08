f59abee59ee6ec2cc0d74dc375bf54eaab42395c
Andrea Zaccheroni <andrea.zaccheroni@gmail.com>
2017-06-08 22:44:55 +0200

    fix

--- resources/views/interventi/edit.blade.php
+++ resources/views/interventi/edit.blade.php
@@ -32,11 +32,11 @@
     <div class="row">
         @include('interventi.partials.textarea')
     </div>
-    @if(Auth::User()->id == $user->id)
+    @if($intervento->inviato == 0 AND Auth::User()->id == $user->id)
         <div class="btn-group btn-block">
             <button type="aggiorna" class="col-md-4 btn btn-success"><i class="fa fa-remove"></i> Aggiorna
             </button>
-            @if($intervento->inviato == 0 AND \Carbon\Carbon::now()->gte(\Carbon\Carbon::parse($intervento->data_start)))
+            @if(\Carbon\Carbon::now()->gte(\Carbon\Carbon::parse($intervento->data_start)))
                 <button type="stampa" value="stampa" class="stampaButton col-md-4 btn btn-primary"><i
                             class="fa fa-calendar"></i>
                     Salva e Stampa
@@ -46,7 +46,6 @@
                 Elimina
             </div>
         </div>
-    @endif
     @elseif($intervento->inviato == 1 )
         <a href="{{ action('InterventoController@show',$intervento->id) }}" type="stampa" value="stampa"
            class="btn-block btn btn-primary"><i class="fa fa-calendar"></i>

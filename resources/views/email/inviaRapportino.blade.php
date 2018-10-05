990a0cbf863fc2895716f149263e437903d93550
Andrea Zaccheroni <andrea.zaccheroni@gmail.com>
2018-10-05 20:16:49 +0200

    mod email rapportino

--- resources/views/email/inviaRapportino.blade.php
+++ resources/views/email/inviaRapportino.blade.php
@@ -1,3 +1,5 @@
 <p>Salve,<br/>
-    In allegato trova il rapportino oggetto dell'intervento {{$intervento->contratto->societa->nome}} del {{$intervento->data}}
+    In allegato trova il rapportino oggetto dell'intervento {{$intervento->contratto->societa->nome}} del {{$intervento->data}} si prega di far ritornare il rapportino con timbro e firma.
+
+    Grazie
 </p>

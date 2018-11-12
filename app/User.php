645082408577efc6544877cbf44400f6d1051b9f
Laravel Shift <shift@laravelshift.com>
2018-11-12 22:31:52 +0000

    Shift core files

--- app/User.php
+++ app/User.php
@@ -2,11 +2,13 @@
 
 namespace App;
 
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+    use Notifiable;
     use SoftDeletes;
     /**
      * The attributes that are mass assignable.

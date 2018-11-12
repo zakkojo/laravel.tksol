8b8c20ed32f9a7b094800f1e4d9ae1cb857e8a05
zakkojo <andrea.zaccheroni@gmail.com>
2018-11-13 00:11:15 +0100

    shift1

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,9 +1,8 @@
 <?php
+
 namespace App\Providers;
 
-use App\Permission;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Contracts\Auth\Access\Gate as GateContract;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -16,28 +15,16 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
     ];
+
     /**
-     * Register any application authentication / authorization services.
+     * Register any authentication / authorization services.
      *
-     * @param  \Illuminate\Contracts\Auth\Access\Gate  $gate
      * @return void
      */
-    public function boot(GateContract $gate)
+    public function boot()
     {
-        $this->registerPolicies($gate);
-        if (Schema::hasTable('permissions')) {
-            foreach ($this->getPermissions() as $permission) {
-                $gate->define($permission->name, function ($user) use ($permission) {
-                    return $user->hasRole($permission->roles);
-                });
-            }
-        }
-    }
+        $this->registerPolicies();
 
-    protected function getPermissions()
-    {
-        if (Schema::hasTable('permissions')) {
-            return Permission::with('roles')->get();
-        }
+        //
     }
 }

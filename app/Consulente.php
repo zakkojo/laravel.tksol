fd9e9aa6654f06dc99f2572a3b49d3dc05f95f9b
zakkojo <andrea.zaccheroni@gmail.com>
2018-11-18 21:53:49 +0100

    fix ::user aggiungendo withTrashed

--- app/Consulente.php
+++ app/Consulente.php
@@ -13,7 +13,7 @@ class Consulente extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
 
     protected $fillable = [

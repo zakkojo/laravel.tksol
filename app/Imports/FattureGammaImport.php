b86874bff917a885f0fa42e287ba589deec4589e
zakkojo <andrea.zaccheroni@gmail.com>
2018-12-10 23:23:24 +0100

    aggiunto filtro import fatture gamma #2038

--- app/Imports/FattureGammaImport.php
+++ app/Imports/FattureGammaImport.php
@@ -21,21 +21,35 @@ class FattureGammaImport implements ToCollection, WithCustomCsvSettings, WithSta
             $intervento = Intervento::find($row[3]);
             if ($intervento)
             {
-                $data_fattura = Carbon::createFromFormat('d/m/Y',$row[2])->setTime(0, 0, 0);
-                $intervento->data_fattura = $data_fattura;
-                $intervento->fatturato = $row[1];
-                $intervento->save();
+                $errore = 0;
+                if ($row[0] == 'HTK-C-PREFAT')
+                {
+                    $errore = 1;
+                    //campo esito = Riga Scartata
+                }
+
+                if ($errore == 0)
+                {
+                    $data_fattura = Carbon::createFromFormat('d/m/Y', $row[2])->setTime(0, 0, 0);
+                    $intervento->data_fattura = $data_fattura;
+                    $intervento->fatturato = $row[1];
+                    $intervento->save();
+                }
             }
-            //else log errore
+            //else esito = Intervento non trovato
         }
     }
-    public function getCsvSettings(): array
+
+    public
+    function getCsvSettings(): array
     {
         return [
             'delimiter' => ';'
         ];
     }
-    public function startRow(): int
+
+    public
+    function startRow(): int
     {
         return 1;
     }

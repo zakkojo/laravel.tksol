5e3f75cd33c0e98cbe9c094791c7160c2cda2e4c
zakkojo@github.com <andrea.zaccheroni@gmail.com>
2021-09-08 17:54:08 +0200

    fix mail auto@tksol.net

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'noreply@tksol.net'),
+        'address' => env('MAIL_FROM_ADDRESS', 'auto@tksol.net'),
         'name' => env('MAIL_FROM_NAME', null),
     ],
 

f0dd66e10a4e38b5a4987b487cac127717d8f816
Andrea Zaccheroni <andrea.zaccheroni@gmail.com>
2022-01-21 10:54:06 +0100

    fix formattazione

--- resources/views/rimborsi/partials/form.blade.php
+++ resources/views/rimborsi/partials/form.blade.php
@@ -2,8 +2,7 @@
     <input type="hidden" name="intervento_id" value="{{$intervento->id}}">
     <div class="form-group">
         <label>Tipo spesa</label>
-        <select id="tipo_spesa" name="tipo_spesa" class="form-control" style="width: 100%;" tabindex="-1"
-                aria-hidden="true">
+        <select id="tipo_spesa" name="tipo_spesa" class="form-control" style="width: 100%;" tabindex="-1" aria-hidden="true">
             <option selected="selected">{{ $rimborso->tipo_spesa or ''}}</option>
             <option>Viaggio</option>
             <option>Pasto</option>
@@ -13,26 +12,24 @@
     <div class="form-group">
         <label>UM</label>
         <div class="input-group">
-            {!! Form::text('um', null,['class'=>'form-control  ', 'id'=>'um', 'placeholder'=>'']) !!}
+            {!! Form::text('um', null,['class'=>'form-control ', 'id'=>'um', 'placeholder'=>'']) !!}
         </div>
     </div>
     <div class="form-group">
         <label>Quantità</label>
         <div class="input-group">
-            {!! Form::text('quantita', null,['class'=>'form-control  pull-right', 'id'=>'quantita', 'placeholder'=>'']) !!}
+            {!! Form::text('quantita', null,['class'=>'form-control pull-right', 'id'=>'quantita', 'placeholder'=>'']) !!}
         </div>
     </div>
     <div class="form-group">
         <label>Importo</label>
         <div class="input-group">
-            {!! Form::text('importo', null,['class'=>'form-control  pull-right', 'id'=>'importo', 'placeholder'=>'']) !!}
+            {!! Form::text('importo', null,['class'=>'form-control pull-right', 'id'=>'importo', 'placeholder'=>'']) !!}
         </div>
     </div>
     <div class="form-group">
         <label>Note</label>
-        <div id="note" name="note" class="wysihtml5"
-             style="width: 100%; height: 150px; font-size: 14px; line-height: 18px; border: 1px solid rgb(221, 221, 221); padding: 10px; "
-             placeholder="">
+        <div id="note" name="note" class="wysihtml5" style="width: 100%; height: 150px; font-size: 14px; line-height: 18px; border: 1px solid rgb(221, 221, 221); padding: 10px; " placeholder="">
             {{ $rimborso->note or '' }}
         </div>
     </div>
@@ -40,32 +37,31 @@
 </div>
 <div id="distanza" class="hidden">{{$intervento->listinoInterventi->contratto->cliente->distanza}}</div>
 @section('page_scripts')
-    <script>
-        $('document').ready(function () {
-            $('.wysihtml5').wysihtml5({
-                toolbar: {
-                    "font-styles": true, //Font styling, e.g. h1, h2, etc. Default true
-                    "emphasis": true, //Italics, bold, etc. Default true
-                    "lists": true, //(Un)ordered lists, e.g. Bullets, Numbers. Default true
-                    "html": false, //Button which allows you to edit the generated HTML. Default false
-                    "link": false, //Button to insert a link. Default true
-                    "image": false, //Button to insert an image. Default true,
-                    "color": false, //Button to change color of font
-                    "blockquote": false, //Blockquote
-                    "size": 'sm' //default: none, other options are xs, sm, lg
-                }
-            });
-
-            $('#tipo_spesa').change(function () {
-                if($('#tipo_spesa').val() == 'Viaggio') {
-                    $('#um').val('Km');
-                    $('#quantita').val($('#distanza').text());
-                }
-                else {
-                    $('#um').val('');
-                    $('#quantita').val('');
-                }
-            });
+<script>
+    $('document').ready(function() {
+        $('.wysihtml5').wysihtml5({
+            toolbar: {
+                "font-styles": true, //Font styling, e.g. h1, h2, etc. Default true
+                "emphasis": true, //Italics, bold, etc. Default true
+                "lists": true, //(Un)ordered lists, e.g. Bullets, Numbers. Default true
+                "html": false, //Button which allows you to edit the generated HTML. Default false
+                "link": false, //Button to insert a link. Default true
+                "image": false, //Button to insert an image. Default true,
+                "color": false, //Button to change color of font
+                "blockquote": false, //Blockquote
+                "size": 'sm' //default: none, other options are xs, sm, lg
+            }
         });
-    </script>
+
+        $('#tipo_spesa').change(function() {
+            if ($('#tipo_spesa').val() == 'Viaggio') {
+                $('#um').val('Km');
+                $('#quantita').val($('#distanza').text());
+            } else {
+                $('#um').val('');
+                $('#quantita').val('');
+            }
+        });
+    });
+</script>
 @append
--- resources/views/rimborsi/partials/indexTable.blade.php
+++ resources/views/rimborsi/partials/indexTable.blade.php
@@ -4,17 +4,14 @@
         <div class="box-tools">
             <div class="btn-group btn-group-sm" role="group" aria-label="...">
                 @if(Auth::User()->id == $user->id OR $user->tipo == 'Partner')
-                    <button type="button" class="btn btn-default"
-                            onClick="location.href='{{ action('RimborsoController@create',$intervento->id) }}'"
-                            title="Aggiungi Nuovo ">
-                        <i class="fa fa-plus"></i>
-                    </button>
+                <button type="button" class="btn btn-default" onClick="location.href='{{ action('RimborsoController@create',$intervento->id) }}'" title="Aggiungi Nuovo ">
+                    <i class="fa fa-plus"></i>
+                </button>
                 @endif
             </div>
             <div class="btn-group btn-group-sm" role="group" aria-label="...">
                 <div class="input-group input-group-sm" style="width: 150px;">
-                    <input type="text" id="clienti_search" name="table_search" class="form-control pull-right"
-                           placeholder="Search">
+                    <input type="text" id="clienti_search" name="table_search" class="form-control pull-right" placeholder="Search">
 
                     <div class="input-group-btn">
                         <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
@@ -27,31 +24,26 @@
 
     <!-- /.box-body -->
     <div class="box-body" style="min-height: 300px;">
-        <table id="clienti" class="table table-striped table-bordered dataTables_wrapper form-inline dt-bootstrap hide"
-               cellspacing="0" width="100%">
+        <table id="clienti" class="table table-striped table-bordered dataTables_wrapper form-inline dt-bootstrap hide" cellspacing="0" width="100%">
             <thead>
-            <tr>
-                <th>Opzioni</th>
-                <th>Tipo Spesa</th>
-                <th>qta</th>
-                <th>um</th>
-                <th>importo</th>
-            </tr>
+                <tr>
+                    <th>Opzioni</th>
+                    <th>Tipo Spesa</th>
+                    <th>qta</th>
+                    <th>um</th>
+                    <th>importo</th>
+                </tr>
             </thead>
             <tbody>
-            @foreach( $rimborsi as $rimborso)
+                @foreach( $rimborsi as $rimborso)
                 <tr>
                     <td>
                         <div class="btn-group btn-group-xs" role="group" aria-label="...">
-                            <button type="button" class="btn btn-default btn-xs"
-                                    onClick="location.href='{{action('RimborsoController@edit',[$intervento->id,$rimborso->id])}}'"
-                                    title="Modifica">
+                            <button type="button" class="btn btn-default btn-xs" onClick="location.href='{{action('RimborsoController@edit',[$intervento->id,$rimborso->id])}}'" title="Modifica">
                                 <i class="glyphicon glyphicon-edit"></i>
                             </button>
-                            <a href="{{ action('RimborsoController@show',[$intervento->id,$rimborso->id]) }}"
-                               data-method="DELETE" data-confirm="Eliminare il rimborso?" data-token="{{csrf_token()}}"
-                               type="button" class="btn btn-danger btn-xs" })">
-                            <i class="glyphicon glyphicon-trash"></i>
+                            <a href="{{ action('RimborsoController@show',[$intervento->id,$rimborso->id]) }}" data-method="DELETE" data-confirm="Eliminare il rimborso?" data-token="{{csrf_token()}}" type="button" class="btn btn-danger btn-xs" })">
+                                <i class="glyphicon glyphicon-trash"></i>
                             </a>
                         </div>
                     </td>
@@ -60,31 +52,31 @@
                     <td>{{$rimborso->um}}</td>
                     <td>{{$rimborso->importo}}</td>
                 </tr>
-            @endforeach
+                @endforeach
             </tbody>
         </table>
     </div>
 </div>
 
 @section('page_scripts')
-    @parent
-    <script>
-        $(document).ready(function () {
-            var oTable = $('#clienti').DataTable({
-                "scrollY": "280px",
-                "scrollCollapse": false,
-                "paging": false,
-                "lengthChange": false,
-                "ordering": true,
-                "info": true,
-                "autoWidth": false,
-                "fnDrawCallback": function () {
-                    $('#clienti').removeClass('hide');
-                }
-            });
-            $('.dataTables_filter').hide();
-            $('#clienti_search').keyup(function () {
-                oTable.search($(this).val()).draw();
-            })
+@parent
+<script>
+    $(document).ready(function() {
+        var oTable = $('#clienti').DataTable({
+            "scrollY": "280px",
+            "scrollCollapse": false,
+            "paging": false,
+            "lengthChange": false,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+            "fnDrawCallback": function() {
+                $('#clienti').removeClass('hide');
+            }
         });
-    </script>@append
+        $('.dataTables_filter').hide();
+        $('#clienti_search').keyup(function() {
+            oTable.search($(this).val()).draw();
+        })
+    });
+</script>@append
